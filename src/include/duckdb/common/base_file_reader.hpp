4adab5b898e61d6c74adefd0569a62c0f83d42bc
Tishj <t_b@live.nl>
2025-03-11 12:25:50 +0100

    undo changes, replace loops with std::find_if

--- src/include/duckdb/common/base_file_reader.hpp
+++ src/include/duckdb/common/base_file_reader.hpp
@@ -106,6 +106,20 @@ public:
 	Value identifier;
 };
 
+struct MultiFileFilterEntry {
+	idx_t index = DConstants::INVALID_INDEX;
+	bool is_constant = false;
+};
+
+struct MultiFileConstantEntry {
+	MultiFileConstantEntry(idx_t column_id, Value value_p) : column_id(column_id), value(std::move(value_p)) {
+	}
+	//! The (global) column id to apply the constant value to
+	idx_t column_id;
+	//! The constant value
+	Value value;
+};
+
 struct MultiFileReaderData {
 	//! The column ids to read from the file
 	//! vector containing a mapping from (indexed with) `local_idx_t` -> `local_column_id_t`
@@ -119,13 +133,14 @@ struct MultiFileReaderData {
 	vector<global_idx_t> column_mapping;
 	//! Whether or not there are no columns to read. This can happen when a file only consists of constants
 	bool empty_columns = false;
-	//! vector containing a mapping from `global_idx_t` -> `local_idx_t` (or DConstants::INVALID_INDEX if constant)
-	vector<local_idx_t> filter_map;
-	//! The set of table filters, which
+	//! Filters can point to either (1) local columns in the file, or (2) constant values in the `constant_map`
+	//! This map specifies where the to-be-filtered value can be found
+	vector<MultiFileFilterEntry> filter_map;
+	//! The set of table filters
 	optional_ptr<TableFilterSet> filters;
-	//! The mapping of (global) index -> constant value
-	unordered_map<global_idx_t, Value> constant_map;
-	//! Map of (local) column_id -> cast, used when reading multiple files when files have diverging types
+	//! The constants that should be applied at the various positions
+	vector<MultiFileConstantEntry> constant_map;
+	//! Map of column_id -> cast, used when reading multiple files when files have diverging types
 	//! for the same column
 	unordered_map<local_column_id_t, LogicalType> cast_map;
 	//! (Optionally) The MultiFileReader-generated metadata corresponding to the currently read file
