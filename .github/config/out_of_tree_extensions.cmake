e820989b811e3c416f43337f28b5b5a2f0697e33
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-06-02 12:55:38 +0200

    uncommect DONT_LINK for mysql scanner

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -159,7 +159,7 @@ duckdb_extension_load(vss
 ################# MYSQL
 if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(mysql_scanner
-            # DONT_LINK
+            DONT_LINK
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-mysql
             GIT_TAG b79ef7e2dde1f9253f9ad584883b029eba8d29a4
